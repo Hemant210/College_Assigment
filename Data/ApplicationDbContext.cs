bb40f5a70c743865ef2ea3ff7a8d0f3bb5688c60
Hemant210 <hemantpatil.2103@gmail.com>
2026-02-06 12:26:47 +0530

    Hemant Add ASii

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationQue2.Models;
+
+
+namespace WebApplication2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<WebApplicationQue2.Models.Employee> Employee { get; set; } = default!;
+
+        
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationQue2.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Display(Name = "Employee Id")]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Employee Name")]
+        public string EmpName { get; set; }
+        public string Project { get; set; }
+
+
+        [Display(Name = "Project Date")]
+        public DateTime ProjectDate { get; set; }
+    }
+}
